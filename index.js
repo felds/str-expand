7516f0a4fd529ed12e9bed6a2f6ce5a78a035507
Felds Liscia <dev@felds.com.br>
2018-12-07 18:08:06 -0200

    refactoring

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { flow, range, flatMap, map, padCharsStart } = require('lodash/fp')
+
 /**
  * Expand a string pattern into a list of strings using sequence or enum tokens.
  * 
@@ -5,47 +7,58 @@
  * @returns {string[]}  The collection of expansions.
  */
 function expand(pattern) {
+    if (pattern === '') {
+        return []
+    }
+
+    // detect braces
+    // https://regex101.com/r/NUZ0mY/2
+    const bracesRegex = /\{([^\}]*?)\}/
+    const bracesMatch = bracesRegex.exec(pattern)
+    if (bracesMatch) {
+        const vals = bracesMatch[1].split(',')
+
+        return pipe(
+            map(v => pattern.replace(bracesRegex, v)),
+            flatMap(s => expand(s))
+        )(vals)
+    }
+
+    // https://regex101.com/r/NUZ0mY/3
+    const bracketsRegex = /\[(\d+)\.\.(\d+)\]/;
+    const bracketsMatch = bracketsRegex.exec(pattern);
+    if (bracketsMatch) {
+        const { 1: from, 2: to } = bracketsMatch
+        const minLength = Math.min(from.length, to.length);
+
+        // @todo unroll this replace? maybe? who knows?
+        // bracketsMatch.start = pattern.slice(0, bracketsMatch.index);
+        // bracketsMatch.end = pattern.slice(bracketsMatch.index + bracketsMatch[0].length);
+
+        console.log(bracketsMatch)
+
+        return pipe(
+            map(padCharsStart("0", minLength)),
+            map(n => pattern.replace(bracketsRegex, n)),
+            flatMap(s => expand(s))
+        )(range(parseInt(from), parseInt(to)).concat(to))
+    }
+
+    return [pattern]
 }
 
 
+const pipe = (...fs) => x => fs.reduce((acc, f) => f(acc), x);
+// const map = f => xs => xs.map(f);
+// const padCharsStart = (char, len) => 
 
-// const { flow, range, flatMap, map, padCharsStart } = require('lodash/fp')
 
-// function strExpand(pattern) {
-//     if (pattern === '') {
-//         return []
-//     }
+// console.log(
+//     range(1, 4).concat(4),
+//     range(1, 4)
+// )
 
-//     // detect braces
-//     // https://regex101.com/r/NUZ0mY/2
-//     const bracesRegex = /\{([^\}]*?)\}/
-//     const bracesMatch = bracesRegex.exec(pattern)
-//     if (bracesMatch) {
-//         const vals = bracesMatch[1].split(',')
-
-//         return flow(
-//             map(v => pattern.replace(bracesRegex, v)),
-//             flatMap(s => strExpand(s))
-//         )(vals)
-//     }
-
-//     // https://regex101.com/r/NUZ0mY/3
-//     const bracketsRegex = /\[(\d+)\.\.(\d+)\]/
-//     const bracketsMatch = bracketsRegex.exec(pattern)
-//     if (bracketsMatch) {
-//         const { 1: from, 2: to } = bracketsMatch
-//         const padding = from < to
-//             ? from.length
-//             : to.length
-
-//         return flow(
-//             map(padCharsStart("0", padding)),
-//             map(n => pattern.replace(bracketsRegex, n)),
-//             flatMap(s => strExpand(s))
-//         )(range(parseInt(from), parseInt(to)).concat(to))
-//     }
-    
-//     return [ pattern ]
+// function expand(pattern) {
 // }
 
 module.exports = expand;
--- index.test.js
+++ index.test.js
@@ -1,71 +1,74 @@
-const expect = require('expect')
-const strExpand = require('./index')
+const expand = require('./index');
 
+test('it expands a simple string to itself', () => {
+    expect(expand('https://example.com'))
+        .toEqual(['https://example.com']);
+})
 
-// return single-item list for strings with no tokens
-expect(strExpand('http://example.com'))
-    .toEqual([ 'http://example.com' ])
+test('it expands an empty string to an empty list', () => {
+    expect(expand(''))
+        .toEqual([]);
+})
 
-// return empty list for empty string
-expect(strExpand(''))
-    .toEqual([])
+test('it expands numerical ranges', () => {
+    expect(expand('http://example.com/[1..3].jpg'))
+        .toEqual([
+            'http://example.com/1.jpg',
+            'http://example.com/2.jpg',
+            'http://example.com/3.jpg',
+        ]);
+})
 
-// expand one range
-expect(strExpand('http://example.com/[1..3].jpg'))
-    .toEqual([
-        'http://example.com/1.jpg',
-        'http://example.com/2.jpg',
-        'http://example.com/3.jpg',
-    ])
+// // expand one range
 
-// expand multiple ranges
-expect(strExpand('http://example.com/[1..2]/[3..4].jpg'))
-    .toEqual([
-        'http://example.com/1/3.jpg',
-        'http://example.com/1/4.jpg',
-        'http://example.com/2/3.jpg',
-        'http://example.com/2/4.jpg',
-    ])
+// // expand multiple ranges
+// expect(strExpand('http://example.com/[1..2]/[3..4].jpg'))
+//     .toEqual([
+//         'http://example.com/1/3.jpg',
+//         'http://example.com/1/4.jpg',
+//         'http://example.com/2/3.jpg',
+//         'http://example.com/2/4.jpg',
+//     ])
 
-// expand enum
-expect(strExpand('banana {bread,muffin,cake}'))
-    .toEqual([
-        'banana bread',
-        'banana muffin',
-        'banana cake',
-    ])
-expect(strExpand('{cold,warm} banana {bread,muffin,cake}'))
-    .toEqual([
-        'cold banana bread',
-        'cold banana muffin',
-        'cold banana cake',
-        'warm banana bread',
-        'warm banana muffin',
-        'warm banana cake',
-    ])
+// // expand enum
+// expect(strExpand('banana {bread,muffin,cake}'))
+//     .toEqual([
+//         'banana bread',
+//         'banana muffin',
+//         'banana cake',
+//     ])
+// expect(strExpand('{cold,warm} banana {bread,muffin,cake}'))
+//     .toEqual([
+//         'cold banana bread',
+//         'cold banana muffin',
+//         'cold banana cake',
+//         'warm banana bread',
+//         'warm banana muffin',
+//         'warm banana cake',
+//     ])
 
-// mix and match
-expect(strExpand('It was called {red,blue,yellow} [3..5]'))
-    .toEqual([
-        'It was called red 3',
-        'It was called red 4',
-        'It was called red 5',
-        'It was called blue 3',
-        'It was called blue 4',
-        'It was called blue 5',
-        'It was called yellow 3',
-        'It was called yellow 4',
-        'It was called yellow 5',
-    ])
+// // mix and match
+// expect(strExpand('It was called {red,blue,yellow} [3..5]'))
+//     .toEqual([
+//         'It was called red 3',
+//         'It was called red 4',
+//         'It was called red 5',
+//         'It was called blue 3',
+//         'It was called blue 4',
+//         'It was called blue 5',
+//         'It was called yellow 3',
+//         'It was called yellow 4',
+//         'It was called yellow 5',
+//     ])
 
-// padded ranges
-expect(strExpand('[009..011]'))
-    .toEqual([ '009', '010', '011' ])
+// // padded ranges
+// expect(strExpand('[009..011]'))
+//     .toEqual([ '009', '010', '011' ])
 
-// decrement
-expect(strExpand('[3..0]'))
-    .toEqual([ '3', '2', '1', '0' ])
+// // decrement
+// expect(strExpand('[3..0]'))
+//     .toEqual([ '3', '2', '1', '0' ])
 
-// padded decrement
-expect(strExpand('[12..08]'))
-    .toEqual([ '12', '11', '10', '09', '08' ])
+// // padded decrement
+// expect(strExpand('[12..08]'))
+//     .toEqual([ '12', '11', '10', '09', '08' ])
