c14dcc36a09d351e5e98ca13674cafd4f98e19d7
Felds Liscia <dev@felds.com.br>
2018-12-08 22:18:45 -0200

    a tiny little refactoring

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { flow, range, flatMap, map, padCharsStart } = require('lodash/fp')
+const { compose, flatMap, map, padCharsStart, range } = require('lodash/fp');
 
 /**
  * Expand a string pattern into a list of strings using sequence or enum tokens.
@@ -8,7 +8,7 @@ const { flow, range, flatMap, map, padCharsStart } = require('lodash/fp')
  */
 function expand(pattern) {
     if (pattern === '') {
-        return []
+        return [];
     }
 
     // detect braces
@@ -16,12 +16,12 @@ function expand(pattern) {
     const bracesRegex = /\{([^\}]*?)\}/
     const bracesMatch = bracesRegex.exec(pattern)
     if (bracesMatch) {
-        const vals = bracesMatch[1].split(',')
+        const values = bracesMatch[1].split(',')
 
-        return pipe(
-            map(v => pattern.replace(bracesRegex, v)),
-            flatMap(s => expand(s))
-        )(vals)
+        return compose(
+            flatMap(s => expand(s)),
+            map(v => pattern.replace(bracesRegex, v))
+        )(values)
     }
 
     // https://regex101.com/r/NUZ0mY/3
@@ -31,34 +31,14 @@ function expand(pattern) {
         const { 1: from, 2: to } = bracketsMatch
         const minLength = Math.min(from.length, to.length);
 
-        // @todo unroll this replace? maybe? who knows?
-        // bracketsMatch.start = pattern.slice(0, bracketsMatch.index);
-        // bracketsMatch.end = pattern.slice(bracketsMatch.index + bracketsMatch[0].length);
-
-        console.log(bracketsMatch)
-
-        return pipe(
-            map(padCharsStart("0", minLength)),
+        return compose(
+            flatMap(s => expand(s)),
             map(n => pattern.replace(bracketsRegex, n)),
-            flatMap(s => expand(s))
-        )(range(parseInt(from), parseInt(to)).concat(to))
+            map(padCharsStart("0", minLength))
+        )([...range(from, to), to ])
     }
 
     return [pattern]
 }
-
-
-const pipe = (...fs) => x => fs.reduce((acc, f) => f(acc), x);
-// const map = f => xs => xs.map(f);
-// const padCharsStart = (char, len) => 
-
-
-// console.log(
-//     range(1, 4).concat(4),
-//     range(1, 4)
-// )
-
-// function expand(pattern) {
-// }
 
 module.exports = expand;
