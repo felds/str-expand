c3fae6e6cf27911172510f5160dfaa146a051ffe
Felds Liscia <dev@felds.com.br>
2021-05-04 12:34:11 -0300

    add prettier config and test script

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const { compose, flatMap, map, padCharsStart, range } = require('lodash/fp');
+const { compose, flatMap, map, padCharsStart, range } = require("lodash/fp");
 
 /**
  * Expand a string pattern into a list of strings using sequence or enum tokens.
- * 
+ *
  * @param {string} pattern  The pattern to be expanded.
  * @returns {string[]}  The collection of expansions.
  */
 function expand(pattern) {
-    if (pattern === '') {
-        return [];
-    }
+  if (pattern === "") {
+    return [];
+  }
 
-    // detect braces
-    // https://regex101.com/r/NUZ0mY/2
-    const bracesRegex = /\{([^\}]*?)\}/
-    const bracesMatch = bracesRegex.exec(pattern)
-    if (bracesMatch) {
-        const values = bracesMatch[1].split(',')
+  // detect braces
+  // https://regex101.com/r/NUZ0mY/2
+  const bracesRegex = /\{([^\}]*?)\}/;
+  const bracesMatch = bracesRegex.exec(pattern);
+  if (bracesMatch) {
+    const values = bracesMatch[1].split(",");
 
-        return compose(
-            flatMap(s => expand(s)),
-            map(v => pattern.replace(bracesRegex, v))
-        )(values)
-    }
+    return compose(
+      flatMap((s) => expand(s)),
+      map((v) => pattern.replace(bracesRegex, v)),
+    )(values);
+  }
 
-    // https://regex101.com/r/NUZ0mY/3
-    const bracketsRegex = /\[(\d+)\.\.(\d+)\]/;
-    const bracketsMatch = bracketsRegex.exec(pattern);
-    if (bracketsMatch) {
-        const { 1: from, 2: to } = bracketsMatch
-        const minLength = Math.min(from.length, to.length);
+  // https://regex101.com/r/NUZ0mY/3
+  const bracketsRegex = /\[(\d+)\.\.(\d+)\]/;
+  const bracketsMatch = bracketsRegex.exec(pattern);
+  if (bracketsMatch) {
+    const { 1: from, 2: to } = bracketsMatch;
+    const minLength = Math.min(from.length, to.length);
 
-        return compose(
-            flatMap(s => expand(s)),
-            map(n => pattern.replace(bracketsRegex, n)),
-            map(padCharsStart("0", minLength))
-        )([...range(from, to), to ])
-    }
+    return compose(
+      flatMap((s) => expand(s)),
+      map((n) => pattern.replace(bracketsRegex, n)),
+      map(padCharsStart("0", minLength)),
+    )([...range(from, to), to]);
+  }
 
-    return [pattern]
+  return [pattern];
 }
 
 module.exports = expand;
